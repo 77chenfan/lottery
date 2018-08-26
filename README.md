4111f1c5b7668e966446214c9f3a8a84deff82c4
77chenfan <77chenfan@gmail.com>
2018-08-26 12:37:52 +0800

    add jia and jian

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # lottery
- 
+
+add 让球数
 .e.g:
 python lottery.py 2016-10-07 011 6
--- lottery.py
+++ lottery.py
@@ -18,17 +18,23 @@ def get_rank(text):
     return rank
 
 class lottery(object):
-	def __init__(self,tr,buyid):
-		self.matchtype = tr['m']
-		self.week = tr['d']
-		wh4 = tr.find('td',class_='wh-4 t-r')
-		self.team1 = wh4.find('a').get_text()
-		self.team1_rank = get_rank(wh4.find('em').get_text())
-		wh6 = tr.find('td',class_='wh-6 t-l')
-		self.team2 = wh6.find('a').get_text()
-		self.team2_rank = get_rank(wh6.find('em').get_text())
-		self.odds = tr.find('input',class_='spArr')['value'].replace('|',' ').split(' ')
-		self.profit = str(float(self.odds[buyid])*(-1))
+    def __init__(self,tr,buyid):
+        self.matchtype = tr['m']
+        self.week = tr['d']
+        wh4 = tr.find('td',class_='wh-4 t-r')
+        self.team1 = wh4.find('a').get_text()
+        self.team1_rank = get_rank(wh4.find('em').get_text())
+        wh6 = tr.find('td',class_='wh-6 t-l')
+        self.team2 = wh6.find('a').get_text()
+        self.team2_rank = get_rank(wh6.find('em').get_text())
+        wh8 = tr.find('td',class_='wh-8 b-l')
+        self.rq=wh8.find('em',class_='rq ').get_text()
+        if(len(tr.find('em',class_='rq jia ')>0)):
+            self.rq2=tr.find('em',class_='rq jia ').get_text()
+        else:
+            self.rq2=tr.find('em',class_='rq jian').get_text()
+        self.odds = tr.find('input',class_='spArr')['value'].replace('|',' ').split(' ')
+        self.profit = str(float(self.odds[buyid])*(-1))
 
 
 if __name__ == '__main__':
@@ -47,5 +53,5 @@ if __name__ == '__main__':
     tr = bs.find('tr',i=index)
     assert tr!=None,"not find %s lottery"%index
     mylo = lottery(tr,buyid)
-    print "%s\t%s vs %s\t%s\t%s vs %s\t%s\t%s"%(mylo.matchtype,mylo.team1,mylo.team2,mylo.week,mylo.team1_rank,mylo.team2_rank,('|').join(mylo.odds),mylo.profit)
-    mylowriter.writerow([mylo.matchtype,mylo.team1+"vs"+mylo.team2,mylo.team1_rank+"vs"+mylo.team2_rank,mylo.week,('|').join(mylo.odds),mylo.profit])
+    print "%s\t%s vs %s\t%s\t%s vs %s\t(%s,%s)\t%s\t%s"%(mylo.matchtype,mylo.team1,mylo.team2,mylo.week,mylo.team1_rank,mylo.team2_rank,mylo.rq,mylo.rq2,('|').join(mylo.odds),mylo.profit)
+    #mylowriter.writerow([mylo.matchtype,mylo.team1+"vs"+mylo.team2,mylo.team1_rank+"vs"+mylo.team2_rank,mylo.week,('|').join(mylo.odds),mylo.profit])
